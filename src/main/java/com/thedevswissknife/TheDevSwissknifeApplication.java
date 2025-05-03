d486e2476e37939878b56169b0f518c0d1c6afe7
rodrigopdo <rodrigo.prudente82@gmail.com>
2025-05-03 14:24:17 -0300

    Initial commit - project structure with Gradle and Spring Boot

---
+++ src/main/java/com/thedevswissknife/TheDevSwissknifeApplication.java
@@ -0,0 +1,12 @@
+package com.thedevswissknife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDevSwissknifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheDevSwissknifeApplication.class, args);
+    }
+}
